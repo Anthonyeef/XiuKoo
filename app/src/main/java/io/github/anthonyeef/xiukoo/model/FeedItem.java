3575be770077fe24a4286b5b1ba222bdccbaab33
Anthonyeef <anthonyeef@gmail.com>
2015-08-24 14:40:41 +0800

    finish the FeedImageView

---
+++ app/src/main/java/io/github/anthonyeef/xiukoo/model/FeedItem.java
@@ -0,0 +1,70 @@
+package io.github.anthonyeef.xiukoo.model;
+
+/**
+ * Created by anthonyeef on 8/22/15.
+ */
+public class FeedItem {
+    private String title, name, source, content, imageUrl, postTime;
+
+    public FeedItem() {
+
+    }
+
+    public FeedItem(String title, String name, String source, String content, String imageUrl, String postTime) {
+        super();
+        this.title = title;
+        this.name = name;
+        this.source = source;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.postTime = postTime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPostTime() {
+        return postTime;
+    }
+
+    public void setPostTime(String postTime) {
+        this.postTime = postTime;
+    }
+}
---
+++ app/src/main/java/io/github/anthonyeef/xiukoo/ui/MainActivity.java
@@ -0,0 +1,58 @@
+package io.github.anthonyeef.xiukoo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.anthonyeef.xiukoo.model.FeedItem;
+import io.github.anthonyeef.xiukoo.adapter.FeedListAdapter;
+import io.github.anthonyeef.xiukoo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ListView listView;
+    private FeedListAdapter listAdapter;
+    private List<FeedItem> feedItems;
+
+    private String URL = "http://www.xiukoo.org";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list);
+        feedItems = new ArrayList<FeedItem>();
+        listAdapter = new FeedListAdapter(this, feedItems);
+        listView.setAdapter(listAdapter);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
